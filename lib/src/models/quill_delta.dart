95741433f831fba9cfd916d58b8ec96914539fc7
milosKarakas <milos.karakas@codebluestudio.com>
2024-02-29 20:44:08 +0100

    - Add prints for debugging

--- lib/src/models/quill_delta.dart
+++ lib/src/models/quill_delta.dart
@@ -357,6 +357,7 @@ class Delta {
     _operations.replaceRange(index - 1, index, [
       Operation._(operation.key, length, resultText, operation.attributes),
     ]);
+    print('result text is $resultText');
   }
 
   /// Pushes new operation into this delta.
@@ -390,6 +391,7 @@ class Delta {
         if (lastOp.hasSameAttributes(operation) &&
             operation.data is String &&
             lastOp.data is String && !(lastOp.data as String).endsWith('\n')) {
+          print('merging with tail ${operation.data} ${operation.attributes}');
           _mergeWithTail(operation);
           return;
         }
@@ -403,9 +405,11 @@ class Delta {
       }
     }
     if (index == _operations.length) {
+      print('adding operation to end ${operation.data} ${operation.attributes}');
       _operations.add(operation);
     } else {
       final opAtIndex = _operations.elementAt(index);
+      print('replacing operation at index $index ${operation.data} ${operation.attributes} ${opAtIndex.data} ${opAtIndex.attributes}');
       _operations.replaceRange(index, index + 1, [operation, opAtIndex]);
     }
     _modificationCount++;
